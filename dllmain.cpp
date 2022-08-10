1d0d89cea237f98c6e63ecf757f6932cb3bbb5b1
Savchik123 <99924072+Savchik123@users.noreply.github.com>
2022-08-10 20:59:35 +0300

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <process.h>
+#include <string>
+bool state = true;
+HINSTANCE self;
+void ews(PVOID)
+{
+	while (true)
+	{
+		if (GetAsyncKeyState(VK_F2))
+		{
+			while (GetAsyncKeyState(VK_F2))
+
+			{
+				Sleep(1);
+			}
+			state = !state;
+		}
+		if (state)
+		{
+			*(char*)(0x5109AC) = 235;
+			*(char*)(0x5109C5) = 235;
+			*(char*)(0x5231A6) = 235;
+			*(char*)(0x52322D) = 235;
+			*(char*)(0x5233BA) = 235;
+		}
+		else
+		{
+			*(char*)(0x5109AC) = 122;
+			*(char*)(0x5109C5) = 122;
+			*(char*)(0x5231A6) = 117;
+			*(char*)(0x52322D) = 117;
+			*(char*)(0x5233BA) = 117;
+		}
+	}
+}
+
+
+
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+	if (fdwReason == DLL_PROCESS_ATTACH)
+	{
+		self = hinstDLL;
+		_beginthread(ews, NULL, NULL);
+	}
+	return TRUE;
+}
